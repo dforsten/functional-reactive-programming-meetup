fa141d4a3cbf43f0d4a55a486009228e03598c2d
davidf <dforsten@gmail.com>
2017-06-13 07:00:12 +0200

    Implemented the "accumulator" example

---
+++ sodium-qt/examples/accumulator/main.cpp
@@ -0,0 +1,60 @@
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QLineEdit>
+
+#include "ExampleMainWindow.h"
+
+#include "SLabel.h"
+#include "SButton.h"
+#include "SLineEdit.h"
+
+using namespace sodium;
+using namespace std;
+
+QString reverse(const QString& s)
+{
+    QString out;
+    for (auto c = s.rbegin(); c != s.rend(); ++c)
+        out += *c;
+    return out;
+}
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    install_debug_message_handler();
+
+    ///////////////////////////////////////////////////////////////////////
+    // Sodium Setup
+
+    SButton* plus = new SButton("+");
+    SButton* minus = new SButton("-");
+
+    stream<int> plus_delta = plus->sClicked.map([](const unit& u) { return 1; });
+    stream<int> minus_delta = minus->sClicked.map([](const unit& u) { return -1; });
+    stream<int> delta = plus_delta.or_else(minus_delta);
+
+    cell_loop<int> value;
+    stream<int> update = delta.snapshot(value, [](int d, int v) { return d + v; });
+    value.loop(update.hold(0));
+
+    SLabel* l1 = new SLabel(value.map([](int i) { return QString::number(i); }));
+
+
+    ///////////////////////////////////////////////////////////////////////
+
+    QHBoxLayout *hLayout = new QHBoxLayout;
+    hLayout->addWidget(l1);
+    hLayout->addWidget(plus);
+    hLayout->addWidget(minus);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(hLayout);
+
+    QWidget *w = new QWidget();
+    w->setLayout(mainLayout);
+
+    ExampleMainWindow mw("simple_hold", w);
+    mw.show();
+    return app.exec();
+}
